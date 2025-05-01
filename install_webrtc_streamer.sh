8d822bca225edd61b3e5d35dd58945c9988dc832
vladus80 <73401455+vladus80@users.noreply.github.com>
2025-05-01 23:12:20 +0300

    Update install_webrtc_streamer.sh

--- install_webrtc_streamer.sh
+++ install_webrtc_streamer.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 set -e  # Остановить при ошибке
 set -u  # Ошибка при использовании неопределённых переменных
 set -o pipefail  # Учитывать ошибки в пайпах
-
-
 APP_DIR="/opt/webrtc-streamer"
 CONFIG_JSON='{
   "urls": {
@@ -13,32 +10,20 @@ CONFIG_JSON='{
     }
   }
 }'
-
-
 echo "=== 1. Обновление системы ==="
 apt update && apt upgrade -y
-
-
 echo "=== 2. Установка зависимостей ==="
 apt install -y wget curl ffmpeg v4l-utils git build-essential cmake libnsl2 libsm6 mc htop
-
-
 echo "=== 3. Скачивание webrtc-streamer ==="
 cd ~
 wget https://github.com/mpromonet/webrtc-streamer/releases/download/v0.8.11/webrtc-streamer-v0.8.11-Linux-x86_64-Release.tar.gz
-
-
 echo "=== 4. Распаковка архива ==="
 tar -xvf webrtc-streamer-v0.8.11-Linux-x86_64-Release.tar.gz
 mv webrtc-streamer-v0.8.11-Linux-x86_64-Release webrtc-streamer
-
-
 echo "=== 5. Копирование файлов в $APP_DIR ==="
 mkdir -p "$APP_DIR"
 cp -r ~/webrtc-streamer/share/webrtc-streamer/* "$APP_DIR/"
 cp ~/webrtc-streamer/bin/webrtc-streamer "$APP_DIR/"
-
-
 echo "=== 6. Создание config.json ==="
 cat > "$APP_DIR/config.json" <<EOF
 {
@@ -49,39 +34,26 @@ cat > "$APP_DIR/config.json" <<EOF
     }
 }
 EOF
-
-
 chmod +x "$APP_DIR/webrtc-streamer"
-
-
 echo "=== 8. Запуск webrtc-streamer ==="
 "$APP_DIR/webrtc-streamer" -C "$APP_DIR/config.json" &
-
-
 echo "=== 9. Создание systemd сервиса ==="
-
 cat <<EOF > /etc/systemd/system/webrtc-streamer.service
 [Unit]
 Description=WebRTC Streamer Service
 After=network.target
-
 [Service]
 ExecStart=$APP_DIR/webrtc-streamer -C $APP_DIR/config.json
 WorkingDirectory=$APP_DIR
 Restart=always
 User=root
-
 [Install]
 WantedBy=multi-user.target
 EOF
-
-
 echo "=== 10. Включение автозапуска через systemd ==="
 systemctl daemon-reexec
 systemctl enable webrtc-streamer
 systemctl start webrtc-streamer
-
-
 echo "=== 11. Проверка статуса службы ==="
 systemctl status webrtc-streamer --no-pager
 
